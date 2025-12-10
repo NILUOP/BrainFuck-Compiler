e504e0a81a24618c44ec06d3441cd88d0ab780e5
NILUOP <nisargkumar2005@gmail.com>
2025-12-10 19:48:38 +0530

    resolved some bugs

--- BrainFuck_compiler.cpp
+++ BrainFuck_compiler.cpp
@@ -1,171 +1,189 @@
 // ===============================================================================================================================================
-
-
-
-
-
-
+// Brainfuck Compiler
+// Author            : Nisarg Chaudhary
+// GitHub Username   : NILUOP
+// Email             : nisargkumar2005@gmail.com
+// Date              : 2025-12-10
+//
+// Description : A simple Brainfuck interpreter written in C++.
+//               - Uses a linear tape of bytes (default size: 30000).
+//               - Supports an optional second argument to extend memory size.
+//               - Reads Brainfuck source code from a .bf file.
+// 
+// Usage       : ./BrainFuck_compiler program.bf [memory_size]
+// 
 // ===============================================================================================================================================
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
+#include <algorithm>
+#include <cstdlib>
 
-#define _Memory_String_Size 30000
+#define Default_Memory_Size 30000
 
 using namespace std;
 
 
-// initialization of _Data_pointer and _Memory_String
-int _Data_Pointer = 0;
-vector<char> _Memory_String;
+// initialization of Data_pointer and Memory_String
+int Data_Pointer = 0;
+vector<char> Memory_String(Default_Memory_Size, 0);
 
 
 // for the execution / compilation of the loops
-int __Compile_BF_Loop(string _Input_String, int _Input_String_Pointer)
+int Compile_BF_Loop(string Input_String, int Input_String_Pointer)
 {
-    // _temp_Input_String_Pointer stores the value of _Input_String_Pointer before entering the loop i.e. location of '[' (or start of loop)
-    int _temp_Input_String_Pointer = _Input_String_Pointer;
-    _Input_String_Pointer++;
-    // _temp_Data_Pointer stores the value of _Data_Pointer before entering the loop
-    int _temp_Data_Pointer = _Data_Pointer;
-    // _Last_Iteration variable is used to ensure that we run full loop before exiting it
-    // there is 3 values of _Last_Iteration variable
-    // _Last_Iteration == 1 -> means we are currently on not on last iteration of the loop and shall run as normal
-    // _Last_Iteration == 2 -> means we are currently on last iteration of the loop
-    // _Last_Iteration == 0 -> means last iteration has been ended and we should stop the loop
-    int _Last_Iteration = 1;
-    while((_Memory_String[_temp_Data_Pointer] != 0 && _Memory_String[_Data_Pointer] != 0) || _Last_Iteration != 0)
+    // Temp_Input_String_Pointer stores the value of Input_String_Pointer before entering the loop i.e. location of '[' (or start of loop)
+    int Temp_Input_String_Pointer = Input_String_Pointer;
+    Input_String_Pointer++;
+    // Temp_Data_Pointer stores the value of Data_Pointer before entering the loop
+    int Temp_Data_Pointer = Data_Pointer;
+    // Last_Iteration variable is used to ensure that we run full loop before exiting it
+    // there is 3 values of Last_Iteration variable
+    // Last_Iteration == 1 -> means we are currently on not on last iteration of the loop and shall run as normal
+    // Last_Iteration == 2 -> means we are currently on last iteration of the loop
+    // Last_Iteration == 0 -> means last iteration has been ended and we should stop the loop
+    int Last_Iteration = 1;
+    while((Memory_String[Temp_Data_Pointer] != 0 && Memory_String[Data_Pointer] != 0) || Last_Iteration != 0)
     {
-        // end of loop if input string have ']'
-        if(_Input_String[_Input_String_Pointer] == ']')
+
+        if(Data_Pointer < 0 || Data_Pointer >= Memory_String.size())
         {
-            if(_Last_Iteration == 2)
+            cerr << "Error: Memory pointer out of bounds!" << endl;
+            exit(1);
+        }
+        // end of loop if input string have ']'
+        if(Input_String[Input_String_Pointer] == ']')
+        {
+            if(Last_Iteration == 2)
             {
-                // last iteration has ended so changing the _Last_Iteration to 0
-                _Memory_String[_Data_Pointer] = 0;
-                _Last_Iteration = 0;
+                // last iteration has ended so changing the Last_Iteration to 0
+                Memory_String[Data_Pointer] = 0;
+                Last_Iteration = 0;
             }
-            // if memory block at _Data_Pointer become become 0 at last part of loop it immediately breaks the loop
-            if(_Memory_String[_Data_Pointer] == 0)
+            // if memory block at Data_Pointer become become 0 at last part of loop it immediately breaks the loop
+            if(Memory_String[Data_Pointer] == 0)
             {
                 break;
             }
-            // sends _Input_String_Pointer back to '['
-            _Input_String_Pointer = _temp_Input_String_Pointer;
+            // sends Input_String_Pointer back to '['
+            Input_String_Pointer = Temp_Input_String_Pointer;
         }
         // incrementing current memory block by 1 if input string have '+'
-        else if(_Input_String[_Input_String_Pointer] == '+')
+        else if(Input_String[Input_String_Pointer] == '+')
         {
-            _Memory_String[_Data_Pointer]++;
+            Memory_String[Data_Pointer]++;
         }
         // decrementing current memory block by 1 if input string have '-'
-        else if(_Input_String[_Input_String_Pointer] == '-')
+        else if(Input_String[Input_String_Pointer] == '-')
         {
-            _Memory_String[_Data_Pointer]--;
+            Memory_String[Data_Pointer]--;
         }
-        // incrementing _Data_Pointer by 1 if input string have '>'
-        else if(_Input_String[_Input_String_Pointer] == '>')
+        // incrementing Data_Pointer by 1 if input string have '>'
+        else if(Input_String[Input_String_Pointer] == '>')
         {
-            _Data_Pointer++;
+            Data_Pointer++;
         }
-        // decrementing _Data_Pointer by 1 if input string have '<'
-        else if(_Input_String[_Input_String_Pointer] == '<')
+        // decrementing Data_Pointer by 1 if input string have '<'
+        else if(Input_String[Input_String_Pointer] == '<')
         {
-            _Data_Pointer--;
+            Data_Pointer--;
         }
         // print out current memory block
-        else if(_Input_String[_Input_String_Pointer] == '.')
+        else if(Input_String[Input_String_Pointer] == '.')
         {
-            cout<<_Memory_String[_Data_Pointer];
+            cout<<Memory_String[Data_Pointer];
         }
         // take input and store at current memory block
-        else if(_Input_String[_Input_String_Pointer] == ',')
+        else if(Input_String[Input_String_Pointer] == ',')
         {
-            cin>>_Memory_String[_Data_Pointer];
+            cin.get(Memory_String[Data_Pointer]);
         }
         // starting of a loop
-        // calling __Compile_BF_Loop
-        else if(_Input_String[_Input_String_Pointer] == '[')
+        // calling Compile_BF_Loop
+        else if(Input_String[Input_String_Pointer] == '[')
         {
-            _Input_String_Pointer = __Compile_BF_Loop(_Input_String, _Input_String_Pointer);
+            Input_String_Pointer = Compile_BF_Loop(Input_String, Input_String_Pointer);
         }
 
-        if ((_Memory_String[_temp_Data_Pointer] == 0 && _Memory_String[_Data_Pointer] != 0) && _Last_Iteration != 0)
+        if ((Memory_String[Temp_Data_Pointer] == 0 && Memory_String[Data_Pointer] != 0) && Last_Iteration != 0)
         {
-            // _Memory_String at _Data_Pointer or at _temp_Data_Pointer has become 0
-            // so changing the _Last_Iteration to 2 to so that program should complete the last iteration of loop
-            _Last_Iteration = 2;                    
+            // Memory_String at Data_Pointer or at Temp_Data_Pointer has become 0
+            // so changing the Last_Iteration to 2 to so that program should complete the last iteration of loop
+            Last_Iteration = 2;                    
         }
-        // incrementing _Input_String_Pointer every time we complete one symbol
-        _Input_String_Pointer++;
+        // incrementing Input_String_Pointer every time we complete one symbol
+        Input_String_Pointer++;
     }
 
     // if the mid way execution of loop stops
     // then this while loop help us to reach end of the loop i.e. ']'
-    while (_Input_String[_Input_String_Pointer] != ']')
+    while (Input_String[Input_String_Pointer] != ']')
     {
-        _Input_String_Pointer++;
+        Input_String_Pointer++;
     }
 
-    return _Input_String_Pointer;
+    return Input_String_Pointer;
 }
 
 
 // function for execution / compiling the BF code
-void __Compile_BF(string _Input_String)
+void Compile_BF(string Input_String)
 {
-    int _Input_String_Pointer = 0;
-    while (_Input_String_Pointer < _Input_String.size())
+    int Input_String_Pointer = 0;
+    while (Input_String_Pointer < Input_String.size())
     {
-        // incrementing current memory block by 1 if input string have '+'
-        if (_Input_String[_Input_String_Pointer] == '+')
+        if(Data_Pointer < 0 || Data_Pointer >= Memory_String.size())
         {
-            _Memory_String[_Data_Pointer]++;
+            cerr << "Error: Memory pointer out of bounds!" << endl;
+            exit(1);
+        }
+        // incrementing current memory block by 1 if input string have '+'
+        if (Input_String[Input_String_Pointer] == '+')
+        {
+            Memory_String[Data_Pointer]++;
         }
         // decrementing current memory block by 1 if input string have '-'
-        else if (_Input_String[_Input_String_Pointer] == '-')
+        else if (Input_String[Input_String_Pointer] == '-')
         {
-            _Memory_String[_Data_Pointer]--;
+            Memory_String[Data_Pointer]--;
         }
-        // incrementing _Data_Pointer by 1 if input string have '>'
-        else if (_Input_String[_Input_String_Pointer] == '>')
+        // incrementing Data_Pointer by 1 if input string have '>'
+        else if (Input_String[Input_String_Pointer] == '>')
         {
-            _Data_Pointer++;
+            Data_Pointer++;
         }
-        // decrementing _Data_Pointer by 1 if input string have '<'
-        else if (_Input_String[_Input_String_Pointer] == '<')
+        // decrementing Data_Pointer by 1 if input string have '<'
+        else if (Input_String[Input_String_Pointer] == '<')
         {
-            _Data_Pointer--;
+            Data_Pointer--;
         }
         // print out current memory block
-        else if (_Input_String[_Input_String_Pointer] == '.')
+        else if (Input_String[Input_String_Pointer] == '.')
         {
-            cout << _Memory_String[_Data_Pointer];
+            cout << Memory_String[Data_Pointer];
         }
         // take input and store at current memory block
-        else if (_Input_String[_Input_String_Pointer] == ',')
+        else if (Input_String[Input_String_Pointer] == ',')
         {
-            cin >> _Memory_String[_Data_Pointer];
+            cin.get(Memory_String[Data_Pointer]);
         }
         // starting of a loop
-        // calling __Compile_BF_Loop
-        else if (_Input_String[_Input_String_Pointer] == '[')
+        // calling Compile_BF_Loop
+        else if (Input_String[Input_String_Pointer] == '[')
         {
-            _Input_String_Pointer = __Compile_BF_Loop(_Input_String, _Input_String_Pointer);
+            Input_String_Pointer = Compile_BF_Loop(Input_String, Input_String_Pointer);
         }
-        // incrementing _Input_String_Pointer every time we complete one symbol
-        _Input_String_Pointer++;
+        // incrementing Input_String_Pointer every time we complete one symbol
+        Input_String_Pointer++;
     }
 }
 
 int main(int argc, char* argv[])
 {
-    ifstream  _FIN(argv[1]);
-    string _Input_String;
-    int _Additional_Memory_String_Size;
-
-
+    
+    
+    int Additional_Memory_String_Size;
     if (argc == 1)
     {
         cerr << "too few arguments" << endl;
@@ -175,20 +193,22 @@ int main(int argc, char* argv[])
     else if(argc > 2)
     {
         // adding extra space as per user input
-        _Additional_Memory_String_Size = max(_Memory_String_Size,(int)argv[2]-_Memory_String_Size);
-        vector<char> _Extra_Memory_String(_Additional_Memory_String_Size);
-        _Memory_String.insert(_Memory_String.end(), _Extra_Memory_String.begin(), _Extra_Memory_String.end());
+        Additional_Memory_String_Size = max(0, atoi(argv[2]) - Default_Memory_Size);
+        vector<char> Extra_Memory_String(Additional_Memory_String_Size);
+        Memory_String.insert(Memory_String.end(), Extra_Memory_String.begin(), Extra_Memory_String.end());
     }
     
+    ifstream  FIN(argv[1]);
+    string Input_String;
     // reading input .bf file and converting it into string
-    if (_FIN.is_open())
+    if (FIN.is_open())
     {
-        string _temp_Input_String;
-        while(getline(_FIN, _temp_Input_String))
+        string Temp_Input_String;
+        while(getline(FIN, Temp_Input_String))
         {
-            _Input_String += _temp_Input_String;
+            Input_String += Temp_Input_String;
         }
-        _FIN.close();
+        FIN.close();
     }
     else
     {
@@ -196,7 +216,7 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    // calling __Compile_BF function for execution
-    __Compile_BF(_Input_String);
+    // calling Compile_BF function for execution
+    Compile_BF(Input_String);
     return 0;
 }
